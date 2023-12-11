f2b693f8d7ff15bc919e88fdd1d8d7f4090e70bf
wangobinicholas <147503433+wangobinicholas@users.noreply.github.com>
2023-12-11 16:30:06 +0300

    Add files via upload

---
+++ B20728_M23B23_031/question3.1.py
@@ -0,0 +1,21 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr  # O(n^2)
+
+
+
+
+
+
+
+
+
+
+
+
